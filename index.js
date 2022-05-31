e365526cb546d68d8a9dbfb901b93992e2c7b168
Bach Le <lehoangbach7802@gmail.com>
2022-05-31 17:07:57 -0400

    js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-document.querySelector('.carousel-container')
+const carouselSlide = document.querySelector('.carousel-container');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+//Buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+//Counter
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform= 'translateX(' +(-size * counter) + 'px)';
+
+//Button Listeners
+
+nextBtn.addEventListener('click', () => {
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter++;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+})
+
+prevBtn.addEventListener('click', () => {
+    carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+    counter--;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+})
+
+//Liker
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
