67cb8891986faa0f9511d949b5fad8a7afef8574
Bach Le <lehoangbach7802@gmail.com>
2022-06-02 17:58:03 -0400

    final

--- index.js
+++ index.js
@@ -1,55 +1,96 @@
-//fetch date from an API
-fetch('https://picsum.photos/v2/list?page=2&limit=5')
+const image1 = document.getElementById('image1')
+let data
+
+fetch('https://picsum.photos/v2/list?page=2&limit=3')
     .then(response => response.json())
     .then(json => {
         console.log(json)
 
-        displayImage(json)
+        image(json)
+        data = json
+
+        const carouselSlide = document.querySelector('.carousel-container');
+        const carouselImages = document.querySelectorAll('img');
+        console.log(carouselImages)
+
+        //Buttons
+        const prevBtn = document.querySelector('#prevBtn');
+        const nextBtn = document.querySelector('#nextBtn');
+
+        //Counter
+
+        let counter = 1;
+        const size = data[0].clientWidth;
+
+        carouselSlide.style.transform= 'translateX(' + (-size * counter) + 'px)';
+
+
+        //Button Listeners
+
+        nextBtn.addEventListener('click', () => {
+        if (counter >= 2) return;
+        carouselSlide.style.transition = "transform 0.4s ease-in-out";
+        counter++;
+        image1.src = data[counter].download_url
+        console.log(counter)
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
     })
 
-//display images
-function displayImage(json){
-    const carousel = document.querySelector(".carousel")
-
-    for(let i=0; i< json.length; i++){
-        const picsUl = document.createElement('ul')
-        const picsLi = document.createElement('li')
-        const pics = document.createElement('img')
-        
-        carousel.appendChild(picsUl)
-
-        picsUl.appendChild(picsLi)
-
-        picsLi.appendChild(pics)
-
-        picsLi.className = "slide"
-        pics.src = json[i].download_url
-    }
-    
-    const firstUl = document.querySelector("ul");
-    const firstLi = document.getElementsByTagName("li")[0];
-    
-    firstUl.setAttribute("data-slides", true)
-    firstLi.setAttribute("data-active", true)
-}
-
-//button
-
-const buttons = document.querySelectorAll("[data-carousel-button]")
-console.log(buttons)
-
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        const offSet = button.dataset.carouselButton === "next" ? 1 : -1
-        
-        const slides = button.closest("[data-carousel]").querySelector("[data-slides]")
-        
-        const activeSlide = slides.querySelector("[data-active]")
-        let newIndex = [...slides.children].indexOf(activeSlide) + offSet
-        if (newIndex < 0) newIndex = slides.children.length - 1
-        if (newIndex >= slides.children.length) newIndex = 0
-
-        slides.children[newIndex].dataset.active = true
-        delete activeSlide.dataset.active
+        prevBtn.addEventListener('click', () => {
+        if (counter <= 0) return;
+        carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+        counter--;
+        image1.src = data[counter].download_url
+        console.log(counter)
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
     })
 })
+
+function image(json){
+    const imageDiv = document.getElementById("image")
+    
+
+    // for(let i=0; i<json.length; i++){
+        //const image = document.createElement('img')
+        image1.src = json[0].download_url
+        //imageDiv.appendChild(image)
+    // }
+}
+
+// document.addEventListener('DOMContentLoaded',()=> {
+//     const carouselSlide = document.querySelector('.carousel-container');
+//     const carouselImages = document.querySelectorAll('.carousel-slide img');
+//     console.log(carouselImages)
+// })
+//create buttons and transitions
+// const carouselSlide = document.querySelector('.carousel-container');
+// const carouselImages = document.querySelectorAll('.carousel-slide img');
+// console.log(carouselImages)
+
+// //Buttons
+// const prevBtn = document.querySelector('#prevBtn');
+// const nextBtn = document.querySelector('#nextBtn');
+
+// //Counter
+
+// let counter = 1;
+// const size = carouselImages[0].clientWidth;
+
+// carouselSlide.style.transform= 'translateX(' +(-size * counter) + 'px)';
+
+
+// //Button Listeners
+
+// nextBtn.addEventListener('click', () => {
+//     if (counter >= carouselImages.length-1) return;
+//     carouselSlide.style.transition = "transform 0.4s ease-in-out";
+//     counter++;
+//     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+// })
+
+// prevBtn.addEventListener('click', () => {
+//     if (counter <= 0) return;
+//     carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+//     counter--;
+//     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+// })
